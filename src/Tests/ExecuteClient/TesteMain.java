0fe05f4718c4896e40ff368d690c42e03f9878ec
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-02-06 23:29:05 -0300

    Main test execute

--- src/Tests/ExecuteClient/TesteMain.java
+++ src/Tests/ExecuteClient/TesteMain.java
@@ -1,7 +1,30 @@
 package Tests.ExecuteClient;
 
+import Tests.Domain.DecoToken;
+import Tests.Domain.TokenT;
+
+import java.util.Scanner;
+
 public class TesteMain {
     public static void main(String[] args) {
+        TokenT token = new TokenT(1);
+        DecoToken decoToken = new DecoToken();
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 1 to create a token \n Enter 2 to read a token");
+        String choiceInput = input.next();
+
+        switch (choiceInput){
+            case "1" :
+                token.insertTokenInfosInitial();
+                token.typeInsert();
+                token.returnToken();
+                break;
+            case "2" :
+                decoToken.insertToken();
+                decoToken.breakToken();
+                break;
+        }
     }
 }
